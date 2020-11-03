1572e16d0d8cbb452e84030cae387a45eb6e5d74
Samuel Aspirin <Samuellyworld@gmail.com>
2020-11-03 04:53:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// GRABBING THE ID(S)
 var h3 = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 function setGradient(){
+// 	SETTING THE GRADIENT
 			body.style.background = "linear-gradient(to right, "
 			+ color1.value +", " + color2.value +
 			")"; 
 			h3.textContent = body.style.background + ";";
 		}
+// ADDING EVENTLISTENERS TO BOTH COLOR1 AND COLOR2
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
